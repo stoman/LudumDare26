6a77eb3f478f2ff1b856fcafc07a39351fd2f75f
Stefan Toman <toman@tum.de>
2013-04-27 13:26:41 +0200

    add update of moving directions by keyboard input

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -25,7 +25,7 @@ $(function() {
 	canvas.width = $('#main-canvas').width();
 
 	context = canvas.getContext('2d');
-	player = new Player(100, 100, 0, 10);
+	player = new Player(100, 100, 0, 10, 0.1);
 	
 	setInterval(draw, 30);
 	setInterval(update, 30);
@@ -51,25 +51,44 @@ function draw() {
 }
 
 function update() {
+	//update positions
 	player.updatePosition();
+	
+	//listen to key events
+	//right: right arrow or d
+	if(-1 != $.inArray(39, keysPressed) || -1 != $.inArray(68, keysPressed) ) {
+		player.turnRight();
+	}
+	//left: left arrow or a
+	if(-1 != $.inArray(37, keysPressed) || -1 != $.inArray(65, keysPressed) ) {
+		player.turnLeft();
+	}
 }
 
-function Player(x, y, angle, speed) {
+function Player(x, y, angle, speed, agility) {
 	this.x = x;
 	this.y = y;
 	this.angle = angle;
 	this.speed = speed;
+	this.agility = agility;
 	
 	this.updatePosition = function() {
 		this.x += this.speed * Math.sin(this.angle);
 		this.y += this.speed * Math.cos(this.angle);
-		this.angle += 0.1;
-	}
+	};
+	
+	this.turnLeft = function() {
+		this.angle += this.agility; 
+	};
+	
+	this.turnRight = function() {
+		this.angle -= this.agility; 
+	};
 	
 	this.render = function() {
 		context.beginPath();
 		context.rect(this.x-5, this.y-5, 10, 10);
 		context.closePath();
 		context.fill();
-	}
+	};
 }
