f3468cadfea780d5258d5917838d3bce5ada8f07
Stefan Toman <toman@tum.de>
2013-04-27 14:18:00 +0200

    add enemies that move towards the player

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,6 +1,7 @@
 var canvas;
 var context;
 var player;
+var enemies = [];
 var keysPressed = [];
 var currentFrame = 0;
 var frameRateStart = 0;
@@ -25,7 +26,23 @@ $(function() {
 	canvas.width = $('#main-canvas').width();
 
 	context = canvas.getContext('2d');
-	player = new Player(100, 100, 0, 10, 0.1);
+	
+	player = new Player(
+		Math.random() * canvas.width,
+		Math.random() * canvas.height,
+		Math.random() * 2 * Math.PI,
+		10,
+		0.1
+	);
+	for(var i = 0; i < 5; i++) {
+		enemies.push(new Enemy(
+			Math.random() * canvas.width,
+			Math.random() * canvas.height,
+			Math.random() * 2 * Math.PI,
+			5 + Math.random(),
+			0.2 * Math.random()
+		));
+	}
 	
 	setInterval(draw, 30);
 	setInterval(update, 30);
@@ -45,6 +62,9 @@ function draw() {
 	
 	//draw elements
 	player.render();
+	for(var i = 0; i < enemies.length; i++) {
+		enemies[i].render();
+	}
 	
 	context.fillText('fps: ' + frameRate, 10, 10);
 	context.fillText('keysPressed: [' + keysPressed + ']', 10, 20);
@@ -53,8 +73,16 @@ function draw() {
 function update() {
 	//update positions
 	player.updatePosition();
+	for(var i = 0; i < enemies.length; i++) {
+		enemies[i].updatePosition();
+	}
 	
-	//listen to key events
+	//turn objects
+	for(var i = 0; i < enemies.length; i++) {
+		enemies[i].turnTowards(player);
+	}
+	
+	//player
 	//right: right arrow or d
 	if(-1 != $.inArray(39, keysPressed) || -1 != $.inArray(68, keysPressed) ) {
 		player.turnRight();
@@ -65,7 +93,54 @@ function update() {
 	}
 }
 
+function normalizeAngle(a) {
+	a %= 2*Math.PI;
+	if(a < 0) {
+		a += 2*Math.PI;
+	}
+	return a;
+}
+
+function copyObject(target, source) {
+	for(var p in source) {
+		target[p] = source[p];
+	} 
+}
+
 function Player(x, y, angle, speed, agility) {
+	copyObject(this, new Thing(x, y, angle, speed, agility));
+	
+	this.render = function() {
+		context.beginPath();
+		context.rect(this.x-5, this.y-5, 10, 10);
+		context.closePath();
+		context.fill();
+	};
+}
+	
+function Enemy(x, y, angle, speed, agility) {
+	copyObject(this, new Thing(x, y, angle, speed, agility));
+	
+	this.render = function() {
+		context.beginPath();
+		context.rect(this.x-2, this.y-2, 4, 4);
+		context.closePath();
+		context.fill();
+		
+		var targetAngle = Math.atan((player.x-this.x) / (player.y-this.y));
+		if(player.y-this.y < 0) {
+			targetAngle += Math.PI;
+		}
+		targetAngle = normalizeAngle(targetAngle);
+		
+		context.beginPath();
+		context.rect(this.x-1 + 10 * Math.sin(targetAngle), this.y-1 + 10 * Math.cos(targetAngle), 2, 2);
+		context.closePath();
+		context.fill();
+	};
+}
+	
+function Thing(x, y, angle, speed, agility) {
 	this.x = x;
 	this.y = y;
 	this.angle = angle;
@@ -86,9 +161,37 @@ function Player(x, y, angle, speed, agility) {
 	};
 	
 	this.render = function() {
-		context.beginPath();
-		context.rect(this.x-5, this.y-5, 10, 10);
-		context.closePath();
-		context.fill();
+		console.log('render not implemented');
+	};
+	
+	this.normalizeAngle = function() {
+		this.angle = normalizeAngle(this.angle);
+	};
+	
+	this.turnTowards = function(target) {
+		this.normalizeAngle();
+		
+		var targetAngle = Math.atan((target.x-this.x) / (target.y-this.y));
+		if(target.y-this.y < 0) {
+			targetAngle += Math.PI;
+		}
+		targetAngle = normalizeAngle(targetAngle);
+		
+		if(targetAngle < Math.PI) {
+			if(this.angle - targetAngle > 0 && this.angle - targetAngle < Math.PI) {
+				this.turnRight();
+			}
+			else {
+				this.turnLeft();
+			}
+		}
+		else {
+			if(targetAngle - this.angle > 0 && targetAngle - this.angle < Math.PI) {
+				this.turnLeft();
+			}
+			else {
+				this.turnRight();
+			}
+		}
 	};
 }
