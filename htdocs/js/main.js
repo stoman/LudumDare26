f93150547568c2251edd761639bf564b7d0f2629
Stefan Toman <toman@tum.de>
2013-04-28 00:29:38 +0200

    refactor code, wait for a click to start the game, include nice fonts

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,13 +1,20 @@
+//canvas element
 var canvas;
 var context;
+
+// game variables
 var player;
 var enemies;
 var highlights;
 var keysPressed;
+
+// frame rates
 var currentFrame;
 var frameRateStart;
 var frameRate;
 var framesNoEnemy;
+
+// sounds
 var sounds = {
     background : [new Audio("sound/background_1.mp3"),
 	    new Audio("sound/background_2.mp3"),
@@ -31,7 +38,11 @@ var soundIndex = {
     player_hit : 0
 };
 var currentBackground;
+
+// cronjobs
 var currentCronjobs = [];
+
+// visuals and colors
 var pointsHistory = 10;
 var colors = {
     player : '#339033',
@@ -47,15 +58,22 @@ var colors = {
     birth : ['#eeffff', '#ddf8f8', '#cceeee', '#bbe8e8', '#aadddd', '#99d7d7',
 	    '#88cccc', '#77c7c7', '#66bbbb', '#55b6b6', '#44aaaa', '#33a6a6',
 	    '#229999', '#119595', '#008888', '#008585'],
-    wall : '#c0c0c0'
+    wall : '#c0c0c0',
+    headings : '#303030'
 };
+
+// level
 var currentLevel = 0;
 var levels;
 
+// state
+var readyToStart = false;
+
 $(function() {
-    levels = [{
-    	backgroundIndex: 0,
-	enemyAgilityBase : 0.05,
+    // create level objects
+    levels = [{// level 1
+	backgroundIndex : 0,
+	enemyAgilityBase : 0.05 - 0.05,
 	enemyAgilityRandom : 0.03,
 	enemySpeedBase : 5,
 	enemySpeedRandom : 1,
@@ -69,9 +87,9 @@ $(function() {
 	new Wall(0, 0, 600, 15),// top
 	new Wall(0, 435, 600, 450)// bottom
 	],
-	winFrames: 30 
-    },{
-    	backgroundIndex: 1,
+	winFrames : 30
+    }, {// level 2
+	backgroundIndex : 1,
 	enemyAgilityBase : 0.1,
 	enemyAgilityRandom : 0.06,
 	enemySpeedBase : 10,
@@ -86,10 +104,12 @@ $(function() {
 	new Wall(0, 0, 600, 15),// top
 	new Wall(0, 435, 600, 450)// bottom
 	],
-	winFrames: 30 
+	winFrames : 30
     }];
     
+    // add listeners
     $(document).keydown(function(e) {
+	startGame();
 	if(-1 == $.inArray(e.keyCode, keysPressed)) {
 	    keysPressed.push(e.keyCode);
 	}
@@ -101,16 +121,21 @@ $(function() {
 	});
 	return false;
     });
+    $('#main-canvas').click(function() {
+	startGame();
+    });
     
+    // prepare canvas
     canvas = document.getElementById('main-canvas');
     canvas.height = $('#main-canvas').height();
     canvas.width = $('#main-canvas').width();
-    
     context = canvas.getContext('2d');
-    startLevel();
+    
+    // load first level
+    loadLevel();
 });
 
-function startLevel() {
+function loadLevel() {
     // create random position
     var pos = randomPosition();
     
@@ -129,15 +154,28 @@ function startLevel() {
     framesNoEnemy = 0;
     
     // add enemies
-    addEnemies(levels[currentLevel].initialEnemies);
+    addEnemies(levels[currentLevel].initialEnemies, false);
     
     // play background music
     currentBackground = playBackgroundMusic(levels[currentLevel].backgroundIndex);
     
-    // add cronjobs
-    currentCronjobs.push(setInterval(draw, 30));
-    setTimeout(function() {currentCronjobs.push(setInterval(update, 30));}, 1200);
-    currentCronjobs.push(setInterval(measureFrameRate, 1000));
+    // mark as ready
+    readyToStart = true;
+    
+    // draw first frame
+    draw();
+}
+
+function startGame() {
+    if(readyToStart) {
+	// add cronjobs
+	currentCronjobs.push(setInterval(draw, 30));
+	setTimeout(function() {
+	    currentCronjobs.push(setInterval(update, 30));
+	}, 1500);
+	currentCronjobs.push(setInterval(measureFrameRate, 1000));
+	readyToStart = false;
+    }
 }
 
 function stopLevel() {
@@ -153,7 +191,7 @@ function stopLevel() {
 
 function nextLevel() {
     currentLevel++;
-    startLevel();
+    loadLevel();
 }
 
 function measureFrameRate() {
@@ -184,16 +222,39 @@ function draw() {
     for( var i = 0; i < highlights.length; i++) {
 	highlights[i].render();
     }
-
-	//texts
-	if(currentFrame <= 30) {
-	    context.fillText('Ready?', 100, 100);
+    
+    // texts
+    if(currentFrame <= 50) {
+	context.font = '60pt Calibri';
+	context.textAlign = 'center';
+	context.textBaseline = 'middle';
+	context.lineWidth = 3;
+	context.strokeStyle = colors.headings;
+	context.strokeText('Level ' + (currentLevel + 1), canvas.width / 2,
+		canvas.height / 3);
+	if(currentFrame <= 1) {
+	    context.font = '30pt Calibri';
+	    context.fillStyle = colors.headings;
+	    context.fillText('Click to start or press any key.',
+		    canvas.width / 2, canvas.height * 2 / 3);
 	}
-	else if(currentFrame <= 40) {
-	    context.fillText('Go!', 100, 100);
+	else if(currentFrame <= 30) {
+	    context.lineWidth = 4;
+	    context.font = '100pt Calibri';
+	    context.strokeText('Ready?', canvas.width / 2,
+		    canvas.height * 2 / 3);
 	}
-
-	//debug messages    
+	else {
+	    context.lineWidth = 5;
+	    context.font = '140pt Calibri';
+	    context.strokeText('Go!', canvas.width / 2, canvas.height * 2 / 3);
+	}
+    }
+    
+    // debug messages
+    context.font = '10pt Calibri';
+    context.textAlign = 'left';
+    context.textBaseline = 'top';
     context.fillStyle = colors.debug;
     context.fillText('fps: ' + frameRate, 10, 10);
     context.fillText('keysPressed: [' + keysPressed + ']', 10, 20);
@@ -231,7 +292,7 @@ function update() {
 	    // remove enemy
 	    enemies.splice(i, 1);
 	    // add enemies
-	    addEnemies(levels[currentLevel].penaltyEnemy);
+	    addEnemies(levels[currentLevel].penaltyEnemy, true);
 	}
     }
     
@@ -243,7 +304,7 @@ function update() {
 	    // add explosion
 	    highlights.push(new Explosion(player.x, player.y));
 	    // add enemies
-	    addEnemies(levels[currentLevel].penaltyWall);
+	    addEnemies(levels[currentLevel].penaltyWall, true);
 	    // adjust player
 	    player.angle += Math.PI;
 	    player.updatePosition();
@@ -280,7 +341,9 @@ function update() {
 	// play sound
 	playSound('enemy_kill');
 	
-	deadEnemies.sort().reverse();
+	deadEnemies.sort(function(a, b) {
+	    return b - a;
+	});
 	for( var i = 0; i < deadEnemies.length; i++) {
 	    // add explosion
 	    highlights.push(new Explosion(enemies[deadEnemies[i]].x,
@@ -306,13 +369,13 @@ function update() {
 	framesNoEnemy++;
     }
     else {
-    	framesNoEnemy = 0;
+	framesNoEnemy = 0;
     }
     
-    //won?
+    // won?
     if(framesNoEnemy >= levels[currentLevel].winFrames) {
-    	stopLevel();
-    	nextLevel();
+	stopLevel();
+	nextLevel();
     }
 }
 
@@ -336,13 +399,13 @@ function normalizeAngle(a) {
     return a;
 }
 
-function addEnemies(n) {
+function addEnemies(n, showHighlight) {
     for( var i = 0; i < n; i++) {
-	addEnemy();
+	addEnemy(showHighlight);
     }
 }
 
-function addEnemy() {
+function addEnemy(showHighlight) {
     // create random position
     var pos = randomPosition();
     
@@ -355,12 +418,17 @@ function addEnemy() {
     enemies.push(enemy);
     
     // add birth highlight
-    highlights.push(new Birth(enemy.x, enemy.y));
+    if(showHighlight) {
+	highlights.push(new Birth(enemy.x, enemy.y));
+    }
 }
 
 function randomPosition() {
+    // compute position
     var pos = new Position(Math.random() * canvas.width, Math.random()
 	    * canvas.height);
+    
+    // check for collisions
     for( var i = 0; i < levels[currentLevel].walls.length; i++) {
 	if(levels[currentLevel].walls[i].collidesWith(pos)) {
 	    // try again
@@ -371,6 +439,7 @@ function randomPosition() {
 }
 
 function copyObject(target, source) {
+    // copy all attributes and methods
     for( var p in source) {
 	target[p] = source[p];
     }
@@ -382,6 +451,7 @@ function Wall(xMin, yMin, xMax, yMax) {
     this.xMax = xMax;
     this.yMax = yMax;
     
+    // render the object
     this.render = function() {
 	context.beginPath();
 	context.rect(this.xMin, this.yMin, this.xMax - this.xMin, this.yMax
@@ -391,6 +461,7 @@ function Wall(xMin, yMin, xMax, yMax) {
 	context.fill();
     };
     
+    // test for collisions
     this.collidesXWith = function(target) {
 	return this.xMin < target.x + target.collisionRadius
 		&& target.x - target.collisionRadius < this.xMax;
@@ -407,8 +478,10 @@ function Wall(xMin, yMin, xMax, yMax) {
 }
 
 function Explosion(x, y) {
+    // extends Highlight
     copyObject(this, new Highlight(x, y));
     
+    // render the object
     this.render = function() {
 	context.beginPath();
 	context.arc(this.x, this.y, Math.ceil((this.step + 1) / 3), 0,
@@ -433,23 +506,29 @@ function Birth(x, y) {
 }
 
 function Highlight(x, y) {
+    // extends Position
     copyObject(this, new Position(x, y));
     
     this.step = colors.explosion.length - 1;
     
+    //compute the object's next position
     this.updatePosition = function() {
 	this.step = Math.max(this.step - 1, 0);
     };
     
+    // render the object
     this.render = function() {
 	console.log('render not implemented');
     };
 }
 
 function Player(x, y, angle, speed, agility) {
+    // extend Thing
     copyObject(this, new Thing(x, y, angle, speed, agility));
+    
     this.collisionRadius = 5;
     
+    // render the object
     this.render = function() {
 	// history
 	for( var i = 0; i < this.xHistory.length; i++) {
@@ -471,8 +550,10 @@ function Player(x, y, angle, speed, agility) {
 }
 
 function Enemy(x, y, angle, speed, agility) {
+    // extend Thing
     copyObject(this, new Thing(x, y, angle, speed, agility));
     
+    // render the object
     this.render = function() {
 	// history
 	for( var i = 0; i < this.xHistory.length; i++) {
@@ -493,6 +574,7 @@ function Enemy(x, y, angle, speed, agility) {
 }
 
 function Thing(x, y, angle, speed, agility) {
+    // extend Position
     copyObject(this, new Position(x, y));
     
     this.xHistory = [];
@@ -502,6 +584,7 @@ function Thing(x, y, angle, speed, agility) {
     this.agility = agility;
     this.collisionRadius = 3;
     
+    //compute the object's next position
     this.updatePosition = function() {
 	// save history
 	this.xHistory.push(this.x);
@@ -518,16 +601,13 @@ function Thing(x, y, angle, speed, agility) {
 	this.y += this.speed * Math.cos(this.angle);
     };
     
+    // turn around
     this.turnLeft = function() {
 	this.angle += this.agility;
     };
     
     this.turnRight = function() {
 	this.angle -= this.agility;
-    };
-    
-    this.render = function() {
-	console.log('render not implemented');
     };
     
     this.normalizeAngle = function() {
@@ -537,12 +617,14 @@ function Thing(x, y, angle, speed, agility) {
     this.turnTowards = function(target) {
 	this.normalizeAngle();
 	
+	// compute angle to target object
 	var targetAngle = Math.atan((target.x - this.x) / (target.y - this.y));
 	if(target.y - this.y < 0) {
 	    targetAngle += Math.PI;
 	}
 	targetAngle = normalizeAngle(targetAngle);
 	
+	// decide in which direction to turn
 	if(targetAngle < Math.PI) {
 	    if(this.angle - targetAngle > 0
 		    && this.angle - targetAngle < Math.PI) {
@@ -563,6 +645,12 @@ function Thing(x, y, angle, speed, agility) {
 	}
     };
     
+    // render the object
+    this.render = function() {
+	console.log('render not implemented');
+    };
+    
+    // test for collisions
     this.collidesWith = function(target) {
 	return this.collisionRadius + target.collisionRadius >= Math.abs(this.x
 		- target.x)
